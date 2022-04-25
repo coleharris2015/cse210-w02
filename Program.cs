d7e6b19a6d880122c430e92d17b391216f8ba16b
coleharris2015 <coleharris2015@gmail.com>
2022-04-25 10:10:50 -0600

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("My name is " + lastName + "," + firstName + " " + lastName); 
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+
+            Console.WriteLine($"Max value" + max);
+
+
+        }
+    }
+}
